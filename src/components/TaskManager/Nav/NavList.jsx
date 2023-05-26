7a0777b34291c0bcdb690042b3bf2c5a06a31d2a
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-05-26 09:52:37 +0200

    Nav update

--- src/components/TaskManager/Nav/NavList.jsx
+++ src/components/TaskManager/Nav/NavList.jsx
@@ -6,7 +6,7 @@ function NavList() {
   const { currentSection, setCurrentSection } = useContext(Context);
 
   return (
-      <div className="flex ml-2 mt-[70px] mb-8 w-full gap-6 tablet:w-auto tablet:m-0 tablet:my-2 tablet:ml-24 tablet:gap-10">
+      <div className="flex ml-2 mt-[70px] mb-10 w-full gap-6 tablet:w-auto tablet:m-0 tablet:my-2 tablet:ml-24 tablet:gap-10">
         <Button
           color="white"
           className={`border-b-4 h-[85%] rounded shadow-xl ${currentSection == "Board" ? "border-[#993a76]" : "border-gray-300 hover:border-gray-400"}`}
