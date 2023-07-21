8649eec8a432379d70b7a57a2642970ee5ef0f79
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-07-21 14:42:51 +0100

    Main changes

--- src/components/TaskManager/Main/MainContent.jsx
+++ src/components/TaskManager/Main/MainContent.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import SideBarBtn from './SideBarBtn'
+import SpaceTitle from './SpaceTitle'
 function MainContent() {
   return (
     <div className='w-full h-full flex flex-wrap gap-4 justify-center items-end rounded-lg bg-white shadow-[0_5px_15px_rgba(0,0,0,.4)] p-4 border-4 border-[#DCDCDC]'>
       <SideBarBtn />
+      <SpaceTitle />
         <div className='w-full h-[90%] flex justify-center items-center text-center rounded-lg bg-white p-4 border-4 border-[#DCDCDC]'>
-          <h1 className='text-4xl font-bold text-[#DCDCDC]'>
+          <button className='text-4xl font-bold text-[#DCDCDC] border-4 border-[#DCDCDC] rounded-lg p-4 border-dashed
+           hover:text-[#c0c0c0] hover:border-[#c0c0c0] transition-all'
+          >
             You haven't selected a space yet
-          </h1>
+          </button>
         </div>
     </div>
   )
---
+++ src/components/TaskManager/Main/SpaceTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SpaceTitle() {
+  return (
+    <h1 className=' font-bold text-3xl text-[#c0c0c0]'>Space Title</h1>
+  )
+}
+
+export default SpaceTitle
