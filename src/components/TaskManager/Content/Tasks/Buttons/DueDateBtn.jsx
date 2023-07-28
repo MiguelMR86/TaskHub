5628a4e97429e8ba08ff44df8b25eb870cc6e28a
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-07-28 22:04:30 +0100

    Due date event added to Tasklist

--- src/components/TaskManager/Content/Tasks/Buttons/DueDateBtn.jsx
+++ src/components/TaskManager/Content/Tasks/Buttons/DueDateBtn.jsx
@@ -5,12 +5,16 @@ import { handelInputDateInsert } from "../../../../../controllers/tasks/function
 import { updateTaskDueDate } from "../../../../../controllers/tasks/functions";
 function DueDateBtn() {
   const { currentTask, setCurrentTask } = useContext(Context);
+  const dueDate = handelInputDateInsert(currentTask.dueDate)
+  const deadline = currentTask.dueDate
+
   return (
     <Input
       label="Due Date"
-      labelProps={{ className: "text-gray-400" }}
+      labelProps={{ className: (deadline < Date.now()) ? "text-red-500" : "text-gray-400" }}
       containerProps={{ className: "min-w-[150px]" }}
-      defaultValue={handelInputDateInsert(currentTask.dueDate)}
+      className={(deadline < Date.now()) ? "text-red-500" : ""}
+      defaultValue={dueDate}
       id="edit-task-due-date"
       type="datetime-local"
       onBlur={(e) => {
--- src/components/TaskManager/Content/Tasks/Buttons/TaskListBtn.jsx
+++ src/components/TaskManager/Content/Tasks/Buttons/TaskListBtn.jsx
@@ -1,26 +1,27 @@
 import React, { useContext } from "react";
 import { Context } from "../../../../../context/Context";
-import { handelStatusColor, handelPriorityColor } from "../../../../../controllers/tasks/functions";
+import {
+  handelStatusColor,
+  handelPriorityColor,
+} from "../../../../../controllers/tasks/functions";
 import { handelDates } from "../../../../../controllers/tasks/functions";
 
 function TaskListBtn({ task }) {
   const { setCurrentTask, handelEditModal } = useContext(Context);
   const statusColor = handelStatusColor(task.status);
   const priorityColor = handelPriorityColor(task.priority);
+  const checkDeadline = task.dueDate > 0 && task.dueDate < Date.now() ? "text-red-500" : "";
+  const checkDeadlineBorder = task.dueDate > 0 && task.dueDate < Date.now() ? "border-red-500 border" : "";
+  const dueDate = task.dueDate == 0 ? "There is no Due Date yet" : handelDates(task.dueDate);
 
-  let date 
-  (task.dueDate == 0) ? date = "There is no Due Date yet" : date = handelDates(task.dueDate)
-  
   return (
     <div
       key={task.id}
       color="white"
-      className='relative w-full h-[50px] mt-8 border border-gray-300 grid place-items-center hover:bg-gray-100 rounded-md hover:shadow-lg'
+      className={`relative w-full h-[50px] mt-8 border border-gray-300 grid place-items-center hover:bg-gray-100 rounded-md hover:shadow-lg ${checkDeadlineBorder}`}
     >
       <div
-        className={`absolute h-[25px] w-[70px] -top-[26px] left-1 text-[11px] text-white rounded-t-md grid place-items-center
-        ${statusColor[1]}
-      `}
+        className={`absolute h-[25px] w-[70px] -top-[25px] left-1 text-[11px] text-white rounded-t-md grid place-items-center ${statusColor[1]} ${checkDeadlineBorder}`}
       >
         <p>{task.status}</p>
       </div>
@@ -30,14 +31,16 @@ function TaskListBtn({ task }) {
           setCurrentTask(task);
           handelEditModal();
         }}
-        className="w-full h-full px-4 flex justify-between items-center"
+        className={`w-full h-full px-4 flex justify-between items-center ${checkDeadline}`}
       >
         <p className="min-w-[149px] text-left truncate">{task.name}</p>
         <div className="flex items-center justify-between h-full max-w-[250px] gap-2 sm:gap-4">
-          <div className={`${priorityColor} text-xs min-w-[70px] h-[30px] rounded-full grid place-items-center text-white`}>
+          <div
+            className={`${priorityColor} text-xs min-w-[70px] h-[30px] rounded-full grid place-items-center text-white`}
+          >
             <p>{task.priority}</p>
           </div>
-          <p className="text-xs text-gray-500">{date}</p>	
+          <p className={`text-xs text-gray-500 ${checkDeadline}`}>{dueDate}</p>
         </div>
       </button>
     </div>
