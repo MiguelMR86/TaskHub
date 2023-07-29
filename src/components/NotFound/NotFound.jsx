3f7765d2019a7a5807defbde06cb226b777a0e2e
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-07-29 20:38:01 +0100

    Not found route

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="w-full h-screen grid place-items-center">
+      <h1 className="text-red-500 text-3xl scale-125">404</h1>
+    </div>
+  );
+}
+
+export default NotFound;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import IndexPage from "../components/Index/IndexPage";
 import TaskManager from "../components/TaskManager/TaskManager";
+import NotFound from "../components/NotFound/NotFound";
 
 const router = createBrowserRouter([
   {
@@ -18,11 +19,7 @@ const router = createBrowserRouter([
   },
   {
     path: "*",
-    element: (
-      <div className="w-full h-screen grid place-items-center">
-        <h1 className="text-red-500 text-3xl scale-125">404</h1>
-      </div>
-    ),
+    element: <NotFound />,
   },
 ]);
 
