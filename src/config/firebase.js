c71292a596e072a3a3398ab2383ed9b1526cb293
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-08-02 15:53:56 +0100

    Env variables ()

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "../../firebase-key";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+const firebaseKey = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PORJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  }
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseKey);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
-export const googleProvider  = new GoogleAuthProvider();
+export const googleProvider = new GoogleAuthProvider();
